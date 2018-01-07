05839e9b70dbee7ba42c23301bdf2c376d5bb5a9
CBLTE <cbrueggenolte@me.com>
2018-01-07 13:58:29 +0100

    change KeyRepeat
    setting KeyRepeat to 0 kinda caused stuttering

--- setup-osx.sh
+++ setup-osx.sh
@@ -110,7 +110,8 @@ defaults -currentHost write -g com.apple.trackpad.trackpadCornerClickBehavior -i
 defaults -currentHost write com.apple.trackpad.enableSecondaryClick -bool true
 
 # Set a really fast keyboard repeat rate.
-defaults write -g KeyRepeat -int 0
+defaults write -g KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
